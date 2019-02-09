fe0e12c94b535e32ce6a918840717023b57292dd
Moiz Ahmed <ahmedmoiz@outlook.com>
2019-02-08 19:25:00 -0500

    Installed App, Set up Homepage View,Applied Migrations

--- SnoozioApp/views.py
+++ SnoozioApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Profile
 # Create your views here.
+class HomePageView(ListView):
+    model = Profile
+    template_name = 'index.html'
